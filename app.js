3eb754ffab375b60891983e7f4b882485745900f
vivcolvmn <vivcolvmn@gmail.com>
2024-08-23 17:11:47 -0700

    changed ES syntax for importing env

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fetch from 'node-fetch';
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = 3000;
